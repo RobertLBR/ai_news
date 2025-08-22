45d8af814b0d1071f0d6770fadd5df4888831993
luborong <luborong@guide-rank.com>
2025-08-22 16:09:27 +0800

    update code

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,5 @@
 FROM ghcr.io/astral-sh/uv:debian-slim
 ADD . /usr/src/app
-
 WORKDIR /usr/src/app
 RUN uv sync
-CMD ["uv run", "backend/server.py"]
+CMD ["sh", "run.sh"]
--- docker_run.sh
+++ docker_run.sh
@@ -1,3 +1,6 @@
 docker build -t ai_news .
 docker ps -a | grep -q ai_news && docker rm -f ai_news
-docker run -d --name ai_news -p 8000:8000 ai_news
+docker run -d --name ai_news \
+-p 8000:8000 \
+-v $(pwd)/data:/usr/src/app/data \
+ai_news
